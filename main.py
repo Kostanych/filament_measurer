e490423f04edbe73dd7949aa387e50eaadf899aa
KOS <kostanych1@gmail.com>
2023-02-16 21:21:06 +0400

    Added PySimpleGUI samples

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import argparse
+import io
 import sys
 
 import cv2
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+import PySimpleGUI as sg
+from PySimpleGUI import *
+from PIL import Image
 
 # from utils import functions
 from utils.functions import measure_length, process_mask, prepare_borders, process_contours
@@ -40,7 +42,7 @@ def load_image_into_numpy_array(path):
     return np.array(cv2.imread(path))
 
 
-def process_image(image='photo_1.jpg', color=True):
+def process_image(image='photo_1.jpg', color=True, verbose=0):
     full_path = "C:\\Users\\KOS\\Documents\\dev\\popeyed_rod_measurer\\data\\input\\photo_1.jpg"
     # print('Running inference for {}... '.format(full_path), end='')
     # _image = np.array(full_path).astype('float32')
@@ -59,138 +61,104 @@ def process_image(image='photo_1.jpg', color=True):
         max_p = 250
         mask = cv2.inRange(image_np, min_p, max_p)
 
-    cv2.imshow('image', image_np)
-    cv2.waitKey(0)
+    if verbose:
+        cv2.imshow('image', image_np)
+        cv2.waitKey(0)
 
-    cv2.imshow('mask', mask)
-    cv2.waitKey(0)
+        cv2.imshow('mask', mask)
+        cv2.waitKey(0)
 
-    print('Start to prosess mask')
-    mask_processed = prepare_borders(mask)
-    mask_processed = process_contours(mask_processed)
-    print('Complete!')
-    cv2.imshow('mask_processed', mask_processed)
-    cv2.waitKey(0)
+        print('Start to prosess mask')
+        mask_processed = prepare_borders(mask)
+        mask_processed = process_contours(mask_processed)
+        print('Complete!')
+        cv2.imshow('mask_processed', mask_processed)
+        cv2.waitKey(0)
 
-    print("Start to compute count of pixels.")
-    measure_length(mask_processed)
+        print("Start to compute count of pixels.")
+        measure_length(mask_processed)
+        print('Done')
+    else:
+        mask_processed = prepare_borders(mask)
+        mask_processed = process_contours(mask_processed)
+        measure_length(mask_processed)
 
     # closing all open windows
     cv2.destroyAllWindows()
 
-    # print(image_np.shape)
-    # print(mask.shape)
-    # print(mask_processed.shape)
-
-    print('Done')
     return mask_processed
 
 
-mask = process_image()
+mask = process_image(verbose=0)
+a = cv2.imwrite(opt.data_path + "C:\\Users\\KOS\\Documents\\dev\\popeyed_rod_measurer\\data\\output\\photo_1.jpg", mask)
+img = mask.copy()
 
 
 
 
+file_types = [("JPEG (*.jpg)", "*.jpg"),
+              ("All files (*.*)", "*.*")]
+def show_image():
+    layout = [
+        [sg.Image(key="-IMAGE-")],
+        [
+            sg.Text("Image File"),
+            sg.Input(size=(25, 1), key="-FILE-"),
+            sg.FileBrowse(file_types=file_types),
+            sg.Button("Load Image"),
+        ],
+    ]
+
+    window = sg.Window("Image Viewer", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        if event == "Load Image":
+            filename = values["-FILE-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE-"])
+                image.thumbnail((400, 400))
+                bio = io.BytesIO()
+                image.save(bio, format="PNG")
+                window["-IMAGE-"].update(data=bio.getvalue())
+
+    window.close()
 
 
-
-# full_path = "C:\\Users\\KOS\\Documents\\dev\\popeyed_rod_measurer\\data\\input\\photo_1.jpg"
-# image_np = load_image_into_numpy_array(full_path)
-# min_p = (0, 0, 0)
-# max_p = (250, 250, 250)
-# mask = cv2.inRange(image_np, min_p, max_p)
-# mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB).T[0]
-# print(mask.astype(bool))
-#
-# all_pixels = mask.shape[0]*mask.shape[1]
-# non_zero = np.count_nonzero(mask)
-# avg_pixels = non_zero/mask.shape[0]
-# print(non_zero)
-# print(all_pixels)
-# print(avg_pixels)
-
-# mask = process_image()
-# test = mask.T
-
-# print(pd.DataFrame(test))
-# for row in test:
-#     #
-#     row = pd.Series(row)
-#     row = row.ne(row.shift()).cumsum()
-#     row = (row != row[0]) & (row != row[row.shape[0] - 1])
-#     row.astype(int).replace(1, 255)
-#     # for row in col:
-#     print(row)
-
-# row_1 = pd.Series([0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
-# row_2 = pd.Series([0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
-# rows_np = np.array([row_1,row_2])
-# print(rows_np)
-#
-# functions.process_mask(rows_np.T)
-
-
-
-
-
-# i 287  row 0
-# i 288  row 0
-# i 289  row 0
-# i 290  row 255
-# i 291  row 255
-# i 292  row 255
-# i 293  row 255
-
-# i 413  row 255
-# i 414  row 255
-# i 415  row 0
-# i 416  row 255
-# i 417  row 0
-# i 418  row 0
-# i 419  row 0
-# i 420  row 255
-# i 421  row 255
-
-
-# img = mask
-# ret, thresh = cv2.threshold(img, 127, 255, 0)
-# contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#
-# cv2.drawContours(img, contours, -1, (0,255,0), 3)
+show_image()
 
 
 #
-# # functions.krutilki(opt.data_path + f"input/photo_1.jpg")
-#
-# # image_np = load_image_into_numpy_array(full_path)
-# img = load_image_into_numpy_array(full_path)
-# image_grayscale = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE)
-#
-# ret, thresh = cv2.threshold(mask, 0, 250, 0, cv2.THRESH_BINARY)
-# # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE) # Use cv2.CCOMP for two level hierarchy
-# #cv2.drawContours(img, contours, -1, (0,255,0), 2)
-#
-# # create an empty mask
-# mask = np.zeros(img.shape[:2], dtype=np.uint8)
-#
-# contours, hierarchy = cv2.findContours(thresh, cv2.RETR_CCOMP,
-#                                        cv2.CHAIN_APPROX_SIMPLE)  # Use cv2.CCOMP for two level hierarchy
-# # loop through the contours
-# for i, cnt in enumerate(contours):
-#
-#     if hierarchy[0][i][3] != -1:  # basically look for holes
-#         # if the size of the contour is less than a threshold (noise)
-#         if cv2.contourArea(cnt) < 70:
-#             # Fill the holes in the original image
-#             cv2.drawContours(img, [cnt], 0, (255), -1)
-#
-# cv2.imshow("Mask", mask)
-#
-# cv2.imshow("Img", img)
-# image = cv2.bitwise_not(img, img, mask=mask)
-# cv2.imshow("Mask", mask)
-# cv2.imshow("After", image)
-#
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-#
+import PySimpleGUI as sg
+import cv2
+def Play():
+    video_name = sg.popup_get_file('Please enter a video name')
+    cap = cv2.VideoCapture(video_name)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+         # Display the resulting frame
+        cv2.imshow('frame',frame)
+         # Press q to close the video windows before it ends
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+layout = [[sg.Button('Play Video')]]
+
+
+window = sg.Window('Video Player', layout)
+while True:
+     event, values = window.read()
+     if event == 'Play Video':
+        Play()
+     elif event == sg.WIN_CLOSED:
+        window.close()
+        break
+
+
+
+
+
