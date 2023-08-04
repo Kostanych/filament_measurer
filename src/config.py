023d86ec4eb208d56d5170d23b93ebe1fbc0a19a
Konstantin Sviblov <konstantin_sviblov@epam.com>
2023-08-04 20:28:21 +0400

    logger added

--- src/config.py
+++ src/config.py
@@ -1,4 +1,15 @@
+"""Configuration holder """
+
+import os
+import sys
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+sys.path.insert(0, os.getcwd())
+PROJECT_DIR = Path(__file__).resolve().parent.parent
+
 
 class Config:
-
     DATA_PATH = ''
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,30 @@
+"""Utilities file"""
+import logging
+import sys
 
-def get_logger():
-    pass
 
+def get_logger(name: str = None, level=logging.INFO):
+    """
+    Sets up the logger handlers for jupyter notebook, ipython or python.
+
+    Separate initialization each time is needed to ensure that logger is set
+    when calling from subprocess
+    (e.g. joblib.Parallel)
+
+    :param name: name of the logger. If None, will return root logger.
+    :param level: Log level (default - INFO)
+    :return: logger with correct handlers
+    """
+    logger = logging.getLogger(name)
+    logger.handlers = []
+    stdout = logging.StreamHandler(sys.stdout)
+    fmt = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+    )
+    stdout.setFormatter(fmt)
+    stdout.setLevel(level)
+    logger.addHandler(stdout)
+    logger.setLevel(level)
+    logger.propagate = False
+    return logger
