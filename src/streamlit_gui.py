1f409591bd1fe34bfc5f11d93ecfd82571d4f318
Konstantin Sviblov <konstantin_sviblov@epam.com>
2023-09-06 19:02:32 +0400

    streamlit wip

--- src/streamlit_gui.py
+++ src/streamlit_gui.py
@@ -19,153 +19,161 @@ st.session_state.play_button_disabled = False
 st.session_state.stop_button_disabled = True
 st.session_state.disabled = False
 
-if 'mask_or_image' not in st.session_state:
-    st.session_state.mask_or_image = st.session_state.get('mask_or_image', 'image')
+if 'cap' not in st.session_state:
+    st.session_state.cap = None
+print('-----------------------------------')
+if 'show_mask' not in st.session_state:
+    print('++++++++++++++++++++++++++++++')
+    print("INIT! No show_mask in session!!")
+    st.session_state.show_mask = False
 if 'show_every_n_frame' not in st.session_state:
     logger.info("show_every_n_frame not in a session state!")
     st.session_state['show_every_n_frame'] = 1
-# Image display area
-vid_area = st.image(st.session_state.title_frame)
+if 'video_path' not in st.session_state:
+    st.session_state['video_path'] = 'Empty'
 
+
+# Functions
+@st.cache_resource
+def get_video_filename():
+    """ Get file name """
+    logger.info('GET FILENAME...')
+    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    filename = (ROOT_DIR + "/data/input/" + st.session_state['filename'])
+    # logger.info(f"st.session_state['filename']  {st.session_state['filename']}")
+    logger.info(f"filename  {os.path.normpath(filename)}")
+    # Return a normalized path.
+    return os.path.normpath(filename)
+
+
+# Function to open video using OpenCV
+def open_video():
+    """ Open a video, return cap into session state """
+    if st.session_state.cap:
+        st.session_state.cap.release()
+    video_path = st.session_state['video_path']
+    st.session_state.cap = cv2.VideoCapture(video_path)
+    # _, st.session_state.title_frame = st.session_state.cap.read()
+
+
+def stop():
+    """ Stop the cap """
+    logger.info(f"BUTTON Stop")
+    st.session_state.play = False
+    if 'cap' in st.session_state:
+        st.session_state.cap.release()
+
+
+def mask_switcher():
+    """ Switcher mask/image """
+    logger.info(f"BUTTON Mask")
+    logger.info(f"Old show_mask:    {st.session_state.show_mask}")
+    st.session_state.show_mask = not st.session_state.show_mask
+    logger.info(f"New show_mask:    {st.session_state.show_mask}")
+
+
+# Elements
 calibration_width = st.sidebar.text_input('Change calibration width (mm):', value=0)
-
 input_source = st.sidebar.selectbox('Input Source', key='input_source',
                                     options=['File', 'USB Device'], index=0)
 video_file = st.sidebar.file_uploader('Select a video file',
-                                      type=['mp4', 'avi', 'mov'])
-
-play_button = st.sidebar.button("Play", key='play_button', )
-stop_button = st.sidebar.button('Stop', key='stop_button')
+                                      type=['mp4', 'avi', 'mov'],
+                                      # on_change=open_video
+                                      )
+play_button = st.sidebar.button("Play", key='play_button', on_click=open_video)
+stop_button = st.sidebar.button('Stop', key='stop_button', on_click=stop)
 show_10_button = st.sidebar.button('Show 10% of frames', key='show_10_button',
                                    disabled=st.session_state.disabled)
 show_100_button = st.sidebar.button('Show 100% of frames', key='show_100_button',
                                     disabled=st.session_state.disabled)
-mask_button = st.sidebar.button('Mask/Image', key='mask_button',
-                                disabled=st.session_state.disabled)
+# mask_radio = st.sidebar.radio("Mask/Image", [":rainbow[Image]", "***Mask***"])
+mask_radio = st.sidebar.radio("Mask/Image", ["Image", "Mask"])
+
+if mask_radio == 'Image':
+    st.session_state.show_mask = False
+else:
+    st.session_state.show_mask = True
 
 
-def change_st_state(st_name, state):
-    st.session_state[st_name] = state
-    logger.info(f"{st_name} - {st.session_state[st_name]}")
+# Image display area
+vid_area = st.image(st.session_state.title_frame)
+st.write(st.session_state)
 
+# set variables
+width_list = []
 
-def switch_mask_and_image():
-    print(f"DDDDDDDDDDDDDDDDDDDDDDDDDDD    {st.session_state['mask_or_image']}")
-    if st.session_state['mask_or_image'] == 'image':
-        st.session_state['mask_or_image'] = 'mask'
-    else:
-        st.session_state['mask_or_image'] = 'image'
-    print(f"NEW     DDDDDDDDDDDDDDDDDDD    {st.session_state['mask_or_image']}")
-
-
-@st.cache_resource
-def load_video_file():
-    logger.info('GET FILENAME...')
-    ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    filename = (ROOT_DIR + "/data/input/" + st.session_state['filename'])
-    logger.info(f"st.session_state['filename']  {st.session_state['filename']}")
-    logger.info(f"filename  {filename}")
-
-    # Show the first frame
-    filename = os.path.normpath(filename)
-    cap = cv2.VideoCapture(filename)
-    return cap
-
-
-def stop(cap):
+if stop_button:
+    """ Stop the cap """
+    logger.info(f"BUTTON Stop")
     st.session_state.play = False
-    cap.release()
-    st.cache_resource.clear()
+    if 'cap' in st.session_state:
+        st.session_state.cap.release()
 
-
-def main():
-    # set variables
-    width_list = []
-    cap = None
-
-    if video_file:
+if video_file:
+    # Get filename, set title frame
+    if 'filename' not in st.session_state:
+        logger.info('filename not in session state')
         st.session_state['filename'] = video_file.name
-        cap = load_video_file()
+        st.session_state['video_path'] = get_video_filename()
+        open_video()
+        _, st.session_state.title_frame = st.session_state.cap.read()
+    else:
+        logger.info('filename IS in session state')
+
+if play_button and video_file:
+    logger.info(f"play_button and video_file is TRUE")
+    st.session_state['play'] = True
+    logger.info(f'play = {st.session_state.play}')
+
+
+print(f"cap:   {st.session_state.cap}")
+print(f"play: {st.session_state.play}")
+if st.session_state.cap and st.session_state.play:
+    print(f"PLAY   {st.session_state['play']}")
+    cap = st.session_state.cap
+    while cap.isOpened():
         ret, frame = cap.read()
-        vid_area.image(frame)
+        if not ret:
+            st.session_state.play = False
+            cap.release()
+            width_list = []
+        if ret:
+            mask, width = process_image(frame=frame, verbose=0)
 
-    if play_button and video_file:
-        cap = load_video_file()
-        # ret, frame = cap.read()
-        # vid_area.image(frame))
-        # if (not st.session_state['play']) & (video_file != '') & (
-        #         input_source == 'File'):
-        #     cap = cv2.VideoCapture(st.session_state['filename'])
-        # elif input_source == 'USB device':
-        #     cap = cv2.VideoCapture(0)
-        st.session_state['play'] = True
-        logger.info(f'cap  = {cap}')
-        logger.info(f'play = {st.session_state.play}')
+            # if st.session_state['mask_or_image'] == 'image':
+            #     source = frame
+            # else:
+            #     source = mask
+            try:
 
-    if stop_button:
-        stop(cap)
-
-    if show_10_button:
-        # Show every 10th frame
-        st.session_state['show_every_n_frame'] = 10
-
-    if show_100_button:
-        # Show all frames
-        st.session_state['show_every_n_frame'] = 1
-
-    if mask_button:
-        logger.debug(f"Before mask/image switcher:   {st.session_state['mask_or_image']}")
-        switch_mask_and_image()
-        logger.debug(f"After mask/image switcher:    {st.session_state['mask_or_image']}")
-
-    if cap and st.session_state.play:
-        print(f"PLAY   {st.session_state['play']}")
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                st.session_state.play = False
-                cap.release()
-                # clear variables
-                st.cache_resource.clear()
-                width_list = []
-            if ret:
-                mask, width = process_image(frame=frame, verbose=0)
-
-                if st.session_state['mask_or_image'] == 'image':
-                    source = frame
-                else:
+                if st.session_state.show_mask:
                     source = mask
-                logger.info(f"SOURCE:   {st.session_state['mask_or_image']}")
-
-                # process frame
-                source, angle = draw_angle_line(source.copy(), mask)
-                angle_multiplier = calculate_pixel_multiplier(angle)
-                width_list.append(width * angle_multiplier)
-
-
-                fps = cap.get(cv2.CAP_PROP_FPS)
-                rolling_1s = mean_rolling(width_list, fps)
-                rolling_10s = mean_rolling(width_list, fps, 10)
-
-                source = draw_fps(source, cap)
-                # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-                vid_area.image(source)
-                time.sleep(1 / fps)  # keep the fps the same as the original fps
-
-            else:
-                # End of video reached
-                cap.release()
-                cap = None
-                # show_play_button = True
-                st.session_state.play = False
-                st.cache_resource.clear()
-                break
+                else:
+                    source = frame
+                logger.info(f"SOURCE IS MASK:   {st.session_state.show_mask}")
+            except Exception as e:
+                print(repr(e))
+                source = frame
 
 
-    st.write('Mean width in pixels: ')
-    st.write('Mean width in mm: ')
+            # process frame
+            source, angle = draw_angle_line(source.copy(), mask)
+            angle_multiplier = calculate_pixel_multiplier(angle)
+            width_list.append(width * angle_multiplier)
 
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            rolling_1s = mean_rolling(width_list, fps)
+            rolling_10s = mean_rolling(width_list, fps, 10)
 
-if __name__ == "__main__":
-    main()
+            source = draw_fps(source, cap)
+            # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            vid_area.image(source)
+            time.sleep(1 / fps)  # keep the fps the same as the original fps
+
+# else:
+#     # End of video reached
+#     st.session_state.play = False
+
+st.write('Mean width in pixels: ')
+st.write('Mean width in mm: ')
